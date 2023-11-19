732dd80d1e11163de8b86e4857bb1c6eff674a9d
Steve <steve.gitprofile@outlook.com>
2023-11-19 16:23:26 +0200

    changed leads to courses

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-const tabBtn = document.getElementById("tab-btn")
+let myCourses = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const coursesFromLocalStorage = JSON.parse(localStorage.getItem("myCourses"));
+const tabBtn = document.getElementById("tab-btn");
 
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
+if (coursesFromLocalStorage) {
+  myCourses = coursesFromLocalStorage;
+  render(myCourses);
 }
 
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
-})
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myCourses.push(tabs[0].url);
+    localStorage.setItem("myCourses", JSON.stringify(myCourses));
+    render(myCourses);
+  });
+});
 
-function render(leads) {
-    let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
-        listItems += `
+function render(courses) {
+  let listItems = "";
+  for (let i = 0; i < courses.length; i++) {
+    listItems += `
             <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
+                <a target='_blank' href='${courses[i]}'>
+                    ${courses[i]}
                 </a>
             </li>
-        `
-    }
-    ulEl.innerHTML = listItems
+        `;
+  }
+  ulEl.innerHTML = listItems;
 }
 
-deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
-})
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myCourses = [];
+  render(myCourses);
+});
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
-})
+inputBtn.addEventListener("click", function () {
+  myCourses.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myCourses", JSON.stringify(myCourses));
+  render(myCourses);
+});
